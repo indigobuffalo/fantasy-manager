6c516ce87289abe82eac2daf6d57154ee887031e
indigobuffalo <stephen.akerson@gmail.com>
2021-03-01 07:58:08 -0800

    WIP: add roster maximizer

---
+++ roster_maximizer.py
@@ -0,0 +1,172 @@
+import yaml
+from pathlib import Path
+from typing import Dict, List
+
+class PositionExhaustedError(Exception):
+
+   def __init__(self, position: str):
+        super().__init__(f"No openings at position '{position}'")
+
+class Player:
+
+    def __init__(self, id: int, name: str, positions: List[str], rank: int):
+        self.id = id
+        self.name = name
+        self.positions = positions
+        self.rank = rank
+
+class RosterDay:
+
+    openings = {
+        'C': 2,
+        'LW': 3,
+        'RW': 3,
+        'D': 4,
+        'UTIL': 1,
+        'G': 2
+    }
+
+    potential_starts = {
+        'C': 0,
+        'LW': 0,
+        'RW': 0,
+        'D': 0,
+        'G': 0
+    }
+
+    positional_scarcity = {
+        'C': 0,
+        'LW': 0,
+        'RW': 0,
+        'D': 0,
+        'G': 0
+    }
+
+    def __init__(self, players_raw: Dict = {}):
+        self.skaters = self.instantiate_players(players_raw['skaters'])
+        self.goalies = self.instantiate_players(players_raw['goalies'])
+        self.populate_potential_starts()
+        self.calculate_positional_scarcity()
+
+    @staticmethod
+    def instantiate_players(players_raw: List[Dict]):
+        players = []
+        for player_rank, player_attrs in players_raw.items():
+            positions = player_attrs['positions']
+            if 'IR' in positions:
+                continue
+
+            name = player_attrs['name']
+            id =  player_attrs['id']
+            players.append(Player(id=id, name=name, positions=positions, rank=player_rank))
+        return players
+
+    @property
+    def center_one(self)
+        return self._center_one
+    
+    @center_one.setter
+    def center_one(self, player: Player)
+        self._center_one = player
+
+    @property
+    def center_two(self)
+        return self._center_two
+    
+    @center_two.setter
+    def center_two(self, player: Player)
+        self._center_two = player
+
+    @property
+    def lw_one(self, player: Player)
+        self._lw_one = player
+
+    @lw_one.setter
+    def lw_one(self, player: Player)
+        self._lw_one = player
+
+    @property
+    def lw_two(self, player: Player)
+        self._lw_two = player
+
+    @lw_two.setter
+    def lw_two(self, player: Player)
+        self._lw_two = player
+
+    @property
+    def lw_three(self, player: Player)
+        self._lw_three = player
+
+    @lw_three.setter
+    def lw_three(self, player: Player)
+        self._lw_three = player
+
+    @property
+    def rw_one(self, player: Player)
+        self._rw_one = player
+
+    @rw_one.setter
+    def rw_one(self, player: Player)
+        self._rw_one = player
+    
+    @property
+    def rw_two(self, player: Player)
+        self._rw_two = player
+
+    @rw_two.setter
+    def rw_two(self, player: Player)
+        self._rw_two = player
+    
+    @property
+    def rw_three(self, player: Player)
+        self._rw_three = player
+
+    @rw_three.setter
+    def rw_three(self, player: Player)
+        self._rw_three = player
+    
+    @property
+    def d_one(self, player: Player)
+        self._d_one = player
+
+    @d_one.setter
+    def d_one(self, player: Player)
+        self._d_one = player
+    
+    def set_positions(self):
+        single_pos_players = [p for p in self.skaters if len(p['position'].split('/')) == 1]
+        dual_pos_players = [p for p in self.skaters if len(p['position'].split('/')) == 2]
+        tri_pos_players = [p for p in self.skaters if len(p['position'].split('/')) == 3]
+
+    
+    def set_position(self, position: str, player: Dict):
+        positions = player['positions']
+        
+        openings_for_position = self.openings[position]
+
+        if openings_for_position == 0:
+            raise PositionExhaustedError(position)
+
+    def populate_potential_starts(self):
+        for player in self.skaters:
+            for position in player.positions:
+                self.potential_starts[position] += 1
+        for player in self.goalies:
+            for position in player.positions:
+                self.potential_starts[position] += 1
+
+    def calculate_positional_scarcity(self):
+        """Positive value indicates surplus, negative value indicates shortage"""
+        self.positional_scarcity['C'] = self.potential_starts['C'] - self.openings['C']
+        self.positional_scarcity['RW'] = self.potential_starts['RW'] - self.openings['RW']
+        self.positional_scarcity['LW'] = self.potential_starts['LW'] - self.openings['LW']
+        self.positional_scarcity['D'] = self.potential_starts['D'] - self.openings['D']
+        self.positional_scarcity['G'] = self.potential_starts['G'] - self.openings['G']
+
+if __name__ == '__main__':
+    project_dir = Path(__file__).parent
+    rosters = Path(project_dir, 'config/rosters')
+    roster = Path(rosters, '2020-02-28_complete.yml')
+    with open(roster) as f:
+        roster_data = yaml.safe_load(f)
+    roster_day = RosterDay(roster_data)
---
+++ update_roster.py
@@ -0,0 +1,106 @@
+#!/Users/Akerson/.local/share/virtualenvs/yahoo-fantasy-1EuG-xYP/bin/python
+
+"""Make a change to your roster.
+
+Usage:
+  update_roster.py waivers --league=<league_id> --locked=<locked_players> --add=<player_id> [--drop=<player_id>]
+  update_roster.py lineup --league=<league_id> --locked=<locked_players> --roster-file=<path_to_roster_file>
+  update_roster.py check --league=<league_id> --locked=<locked_players> --check=<players_to_check>
+
+Options:
+  --drop=<player_id>    Id of player to drop.
+
+"""
+
+import os
+import json
+import requests
+import yaml
+from datetime import datetime
+from pathlib import Path
+from typing import List
+
+from docopt import docopt
+
+from headers import COOKIE
+
+
+YAHOO_FANTASY_URL = 'https://hockey.fantasysports.yahoo.com/hockey'
+PROJECT_DIR = Path(__file__).parent.absolute()
+CRUMB="qOM0LqGpJSr"
+
+class RosterController:
+    
+    def __init__(self, league_id: str, locked_players: List):
+        self.team_url = f'{YAHOO_FANTASY_URL}/{league_id}/1'
+        self.locked_players = locked_players
+        self.rosters_dir = PROJECT_DIR / 'rosters'
+
+        self.session = requests.Session()
+        self.session.headers.update({ 'cookie': COOKIE })
+        import ipdb; ipdb.set_trace()
+
+    def check_current_auth(self):
+        resp = self.session.get(self.team_url)
+        if not all (player in resp.text for player in self.locked_players):
+            raise RuntimeError('Not logged in!')
+    
+    def on_roster(self, player_id):
+        team_response = self.session.get(self.team_url)
+        return player_id in team_response.text
+
+    def add_player(self, add_id: str, drop_id: str = None):
+        if self.on_roster(add_player_id):
+            return "Already added player!"
+        data = {
+            'stage': '3',
+            'crumb': f'{CRUMB}',
+            'stat1': 'S',
+            'stat2': 'D',
+            'apid': add_id,
+        }
+        if drop_id is not None:
+            data['dpid'] = drop_id
+        add_response = self.session.post(f'{self.team_url}/addplayer?apid={add_id}', data=data)
+        if "player has already played and is no longer" in add_response.text:
+            raise ValueError("Player to drop has played today - try tomorrow!")
+        if "You have reached the weekly limit" in add_response.text:
+            raise RuntimeError("Already reached max adds for the week!")
+        if not self.on_roster(add_player_id):
+            raise RuntimeError(f"Something went wrong - player {add_id} was not added to roster!")
+        return add_response
+
+    def edit_lineup(self, roster_filename: str):
+        data = {
+            "date": datetime.strftime(datetime.now(), '%Y-%m-%d'),
+            "stat1": "S",
+            "stat2": "D",
+            'crumb': f'{CRUMB}',
+            "ret": "swap"
+        }
+        with open(self.rosters_dir / f'{roster_filename}.yml') as roster_file:
+            roster = yaml.safe_load(roster_file)
+        data.update(roster)
+        return self.session.post(f'{self.team_url}/editroster', data=data)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    league_id = args['--league']
+    locked_players = args['--locked'].split(',')
+
+    controller = RosterController(league_id, locked_players)
+    controller.check_current_auth()
+
+    if args['check']:
+        players_to_check = args['--check'].split(',')
+        for player in players_to_check:
+            if not controller.on_roster(player):
+                raise RuntimeError(f"Player '{player}' has not been added!")
+        print(f'All players on roster - {players_to_check}')
+    if args['lineup']:
+        resp = controller.edit_lineup(args['--roster-file'])
+    if args['waivers']:
+        add_player_id = args['--add']
+        controller.add_player(add_player_id, args['--drop'])
+        
